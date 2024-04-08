dd55d24f615a77664754e30efdac23b301e6561c
dir-FY24izumitakuma <takuma.izumi.l696@dirbato.co.jp>
2024-04-08 17:57:13 +0900

    最初のコミット

---
+++ Sample/src/sample/IfSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class IfSample {
+
+    public static void main(String[] args) {
+        var score=50;//成績
+        
+        if(score>=60) {
+            System.out.println("合格です！");
+        }else {
+            System.out.println("赤点です。。。");
+        }
+    }
+
+}
---
+++ Sample/src/sample/Test.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Test {
+    //mainメソッドはプログラミングの最初に動作します
+    public static void main(String[] args) {
+        /*コメントアウト
+        System.out.println("kiramex");
+        System.out.println("Java");
+        */
+        System.out.println("学習を始めましょう！");
+    }
+
+}
